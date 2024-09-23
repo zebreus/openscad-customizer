0fcd187a89d97749c14c36f1f0867c8e444bd566
Lennart <zebreus@zebre.us>
2024-09-23 23:25:28 +0000

    Switch editor for customizer in the initial state

--- src/state/initial-state.ts
+++ src/state/initial-state.ts
@@ -1,88 +1,94 @@
 // Portions of this file are Copyright 2021 Google LLC, and licensed under GPL2+. See COPYING.
 
-import defaultScad from './default-scad';
-import { State } from './app-state';
+import defaultScad from "./default-scad";
+import { State } from "./app-state";
 
-export const defaultSourcePath = '/playground.scad';
-export const defaultModelColor = '#f9d72c';
-  
+export const defaultSourcePath = "/playground.scad";
+export const defaultModelColor = "#f9d72c";
+
 export const blankProjectState: State = {
   params: {
     activePath: defaultSourcePath,
-    sources: [{path: defaultSourcePath, content: ''}],
+    sources: [{ path: defaultSourcePath, content: "" }],
     features: [],
-    renderFormat: 'glb',
-    exportFormat: 'glb',
+    renderFormat: "glb",
+    exportFormat: "glb",
   },
   view: {
     color: defaultModelColor,
     layout: {
-      mode: 'single',
-      focus: 'editor'
-    }
-  }
+      mode: "single",
+      focus: "editor",
+    },
+  },
 };
 
 export function createInitialState(fs: any, state: State | null) {
-
-  type Mode = State['view']['layout']['mode'];
-  const mode: Mode = window.matchMedia("(min-width: 768px)").matches 
-    ? 'multi' : 'single';
+  type Mode = State["view"]["layout"]["mode"];
+  const mode: Mode = window.matchMedia("(min-width: 768px)").matches
+    ? "multi"
+    : "single";
 
   const initialState: State = {
     params: {
       activePath: defaultSourcePath,
-      sources: [{path: defaultSourcePath, content: defaultScad}],
+      sources: [{ path: defaultSourcePath, content: defaultScad }],
       features: [],
-      renderFormat: 'glb',
-      exportFormat: 'glb',
+      renderFormat: "glb",
+      exportFormat: "glb",
       extruderColors: state?.params?.extruderColors,
     },
     view: {
       layout: {
-        mode: 'multi',
-        editor: true,
+        mode: "multi",
+        editor: false,
         viewer: true,
-        customizer: false,
+        customizer: true,
       } as any,
 
       color: defaultModelColor,
     },
-    ...(state ?? {})
+    ...(state ?? {}),
   };
 
-  if (initialState.view.layout.mode != mode) {
-    if (mode === 'multi' && initialState.view.layout.mode === 'single') {
-      initialState.view.layout = {
-        mode,
-        editor: true,
-        viewer: true,
-        customizer: initialState.view.layout.focus == 'customizer'
-      }
-    } else if (mode === 'single' && initialState.view.layout.mode === 'multi') {
-      initialState.view.layout = {
-        mode,
-        focus: initialState.view.layout.viewer ? 'viewer'
-          : initialState.view.layout.customizer ? 'customizer'
-          : 'editor'
-      }
-    }
-  }
+  // // This changes the default state to viewer on mobile. I dont want that for now
+  // if (initialState.view.layout.mode != mode) {
+  //   if (mode === 'multi' && initialState.view.layout.mode === 'single') {
+  //     initialState.view.layout = {
+  //       mode,
+  //       editor: true,
+  //       viewer: true,
+  //       customizer: initialState.view.layout.focus == 'customizer'
+  //     }
+  //   } else if (mode === 'single' && initialState.view.layout.mode === 'multi') {
+  //     initialState.view.layout = {
+  //       mode,
+  //       focus: initialState.view.layout.viewer ? 'viewer'
+  //         : initialState.view.layout.customizer ? 'customizer'
+  //         : 'editor'
+  //     }
+  //   }
+  // }
 
-  initialState.view.showAxes ??= true
-  initialState.view.showShadows ??= true
+  initialState.view.showAxes ??= true;
+  initialState.view.showShadows ??= true;
 
   // fs.writeFile(initialState.params.sourcePath, initialState.params.source);
   // if (initialState.params.sourcePath !== defaultSourcePath) {
   //   fs.writeFile(defaultSourcePath, defaultScad);
   // }
-  
-  const defaultFeatures = ['manifold', 'fast-csg', 'lazy-union', 'assimp', 'render-colors'];
-  defaultFeatures.forEach(f => {
+
+  const defaultFeatures = [
+    "manifold",
+    "fast-csg",
+    "lazy-union",
+    "assimp",
+    "render-colors",
+  ];
+  defaultFeatures.forEach((f) => {
     if (initialState.params.features.indexOf(f) < 0)
-    initialState.params.features.push(f);
+      initialState.params.features.push(f);
   });
 
   return initialState;
 }
-
